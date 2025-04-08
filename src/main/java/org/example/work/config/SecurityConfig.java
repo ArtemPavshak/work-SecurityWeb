8a12ca6214efbb870460c293cb30369749c99d6c
ArtemPavshak <124794623+ArtemPavshak@users.noreply.github.com>
2025-04-08 17:43:36 +0300

    CSRF

---
+++ src/main/java/org/example/work/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+/*
+  @author   rakel
+  @project   work
+  @class  SecurityConfig
+  @version  1.0.0 
+  @since 08.04.2025 - 17.33
+*/
+package org.example.work.config;/*
+  @author   rakel
+  @project   work
+  @class  SecurityConfig
+  @version  1.0.0 
+  @since 08.04.2025 - 17.33
+*/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
